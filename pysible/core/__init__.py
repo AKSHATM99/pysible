5ce29fb23fa1493295b1ebe8fb7e87c6ba82cc76
Akshat Mishra <mishra.akshat27sep@gmail.com>
2025-09-12 00:56:24 +0530

    Created PyRate

--- pysible/core/__init__.py
+++ pysible/core/__init__.py
@@ -1,8 +1,9 @@
 from .rbac import RBAC
 from .auth import Auth
 from .token import Token
+from .pyrate import PyRate
 # from .pyrate import Pyrate  # if you have this too
 
 # optional: define __all__ for star imports
-__all__ = ["RBAC", "Auth", "Token"]
+__all__ = ["RBAC", "Auth", "Token", "PyRate"]
 
